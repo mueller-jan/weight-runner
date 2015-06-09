460e6affb96471d1590cdb26b5bc83c62e288d64
mueller-jan <mueller.jan.87@gmail.com>
2015-06-09 15:34:46 +0200

    changed phaser version to 2.2.2 due to tilesprite collision bug (https://github.com/photonstorm/phaser/issues/263) in phaser added jump-functionality

--- js/game/states/Game.js
+++ js/game/states/Game.js
@@ -2,6 +2,8 @@
 
 Runner.Game = function (game) {
     console.log('init');
+    this.player;
+    this.ground;
 };
 var cursors;
 Runner.Game.prototype = {
@@ -21,21 +23,22 @@ Runner.Game.prototype = {
         this.background.autoScroll(-100,0);
 
         //Boden
-        this.ground = this.game.add.tileSprite(0,this.game.height - 85, this.game.width, 85, 'ground');
+        this.ground = this.game.add.tileSprite(0,this.game.height - 73, this.game.width, 73, 'ground');
         this.ground.autoScroll(-400,0);
+
         this.game.physics.arcade.enableBody(this.ground);
         this.ground.body.allowGravity = false;
         this.ground.body.immovable = true;
-        this.ground.enableBody = true;
 
         //Player
-        this.player = this.game.add.sprite(32, this.game.height - 85 - 45, 'player');
-        this.game.physics.arcade.enableBody(this.player);
+        this.player = this.game.add.sprite(32, this.game.height - 85 - 100, 'player');
+
         //this.player.animations.add('left', [0, 1, 2, 3], 10, true);
         this.player.animations.add('right', [5, 6, 7, 8], 10, true);
         this.player.animations.play('right', 8, true);
+
+        this.game.physics.arcade.enableBody(this.player);
         this.player.body.collideWorldBounds = true;
-        this.player.body.bounce.setTo(0.25, 0.25);
 
         this.cursors = this.game.input.keyboard.createCursorKeys();
     },
@@ -43,6 +46,7 @@ Runner.Game.prototype = {
     update: function () {
         //funktioniert nicht
         this.game.physics.arcade.collide(this.player, this.ground);
+
         if (this.cursors.left.isDown)
         {
             //nach links
@@ -53,7 +57,15 @@ Runner.Game.prototype = {
             //nach rechts
             this.player.body.velocity.x = 150;
         }
+
+        //springen - nur möglich, wenn der Spieler den Boden berührt
+        if (this.cursors.up.isDown && this.player.body.touching.down)
+        {
+            this.player.body.velocity.y = -350;
+        }
     },
+
     render: function() {
+       // this.game.debug.body(this.ground);
     }
 };
